90b3751861bc43aa281c8fec09cad06bf427a971
matlazar <matijalazar@yahoo.com>
2016-11-09 20:49:59 +0100

    Napravljen fragment i klasa O nama
    Završen dizajn i poziv layout-a u klasi OnamaFragment. Potrebno je
    dodati ovaj fragment u navigaciju te dodati smisleni tekst o nama koji
    je trenutno nedostupan jer je i sama stranica autoškole nedostupna

---
+++ aplikacija/app/src/main/java/com/bkl/air/foi/mdrivingschool/OnamaFragment.java
@@ -0,0 +1,25 @@
+package com.bkl.air.foi.mdrivingschool;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by HP on 9.11.2016..
+ */
+
+public class OnamaFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_o_nama,container,false);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+    }
+
+}
