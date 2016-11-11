b4319fefe4df0eec4103feb57510065846e349a7
matlazar <matijalazar@yahoo.com>
2016-11-11 14:42:14 +0100

    ViewFlipper napravljen u potpunosti
    ViewFlipper napravljen u potpunosti, još je potreno napraviti do kraja
    dizajn početne strane. Da bi se to ostvarilo potrebno je dizajnirati
    gumbove koje će voditi na fragmente koji su dostupni i prek menu-a.

--- aplikacija/app/src/main/java/com/bkl/air/foi/mdrivingschool/MainScreenFragment.java
+++ aplikacija/app/src/main/java/com/bkl/air/foi/mdrivingschool/MainScreenFragment.java
@@ -2,9 +2,17 @@ package com.bkl.air.foi.mdrivingschool;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.transition.Fade;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ViewFlipper;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by Dalibor on 9.11.2016..
@@ -12,9 +20,27 @@ import android.view.ViewGroup;
 
 public class MainScreenFragment extends Fragment {
 
+    Animation Fade_in, Fade_out;
+    private Unbinder unbinder;
+
+    @BindView(R.id.bckgrndViewFlipper)
+    ViewFlipper viewFlipper;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_screen_fragment, container, false);
+        View calcView = inflater.inflate(R.layout.main_screen_fragment, container, false);
+        unbinder = ButterKnife.bind(this,calcView);
+
+        Fade_in = AnimationUtils.loadAnimation(getActivity(),android.R.anim.fade_in);
+        Fade_out = AnimationUtils.loadAnimation(getActivity(),android.R.anim.fade_out);
+
+        viewFlipper.setInAnimation(Fade_in);
+        viewFlipper.setOutAnimation(Fade_out);
+
+        viewFlipper.setAutoStart(true);
+        viewFlipper.setFlipInterval(4000);
+        viewFlipper.startFlipping();
+        return calcView;
     }
 
     @Override
