1fe37178278b242101dd1c1f1d5b8f62c62f8da5
matlazar <matijalazar@yahoo.com>
2016-11-11 16:22:55 +0100

    Završen dizajn početnog zaslona
    Napravljen je dizajn početnog zaslona. Za Image Buttone su stavljene
    ikone koje se nalaze i u navigaciju. Gumbovi su u potpunosti
    funkcionalni i otvaraju odgovarajuće fragmente

--- aplikacija/app/src/main/java/com/bkl/air/foi/mdrivingschool/MainScreenFragment.java
+++ aplikacija/app/src/main/java/com/bkl/air/foi/mdrivingschool/MainScreenFragment.java
@@ -1,6 +1,7 @@
 package com.bkl.air.foi.mdrivingschool;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.transition.Fade;
 import android.view.LayoutInflater;
@@ -8,10 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 import android.widget.ViewFlipper;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 /**
@@ -25,6 +28,15 @@ public class MainScreenFragment extends Fragment {
 
     @BindView(R.id.bckgrndViewFlipper)
     ViewFlipper viewFlipper;
+
+    @BindView(R.id.imageButton)
+    ImageButton imageButton1;
+
+    @BindView(R.id.imageButton2)
+    ImageButton imageButton2;
+
+    @BindView(R.id.imageButton3)
+    ImageButton imageButton3;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -46,5 +58,35 @@ public class MainScreenFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    @OnClick(R.id.imageButton)
+    public void onButtonImageClick1(){
+        OnlinePrijavaFragment fop = new OnlinePrijavaFragment();
+        FragmentTransaction fm = getFragmentManager().beginTransaction();
+        fm.replace(R.id.fragment_container, fop);
+        fm.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fm.addToBackStack("Online prijava     ");
+        fm.commit();
+    }
+
+    @OnClick(R.id.imageButton2)
+    public void onButtonImageClick2(){
+        KontaktFragment fk = new KontaktFragment();
+        FragmentTransaction fm = getFragmentManager().beginTransaction();
+        fm.replace(R.id.fragment_container, fk);
+        fm.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fm.addToBackStack("Kontakt     ");
+        fm.commit();
+    }
+
+    @OnClick(R.id.imageButton3)
+    public void onButtonImageClick3(){
+        VozilaFragment fv = new VozilaFragment();
+        FragmentTransaction fm = getFragmentManager().beginTransaction();
+        fm.replace(R.id.fragment_container, fv);
+        fm.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fm.addToBackStack("vozila   ");
+        fm.commit();
     }
 }
