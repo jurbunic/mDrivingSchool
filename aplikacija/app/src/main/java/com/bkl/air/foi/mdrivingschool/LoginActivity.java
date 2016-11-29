39c0898cdf9f555f48ddf53f3ea84b8563794d79
matlazar <matijalazar@yahoo.com>
2016-11-29 17:50:08 +0100

    LoginActivity autentikacija
    Logika autentikacije je napravljena do kraja. Gumb na LoginActivityu je
    funkcionalan. Potrebno je napraviti Activity-e za zaposlenika i
    polaznika da se napravi redirekcija na njih u slučaju uspješne prijave.
    Trenutno se pri uspješnoj/neuspješnoj prijavi prikazuju AlertDialog.

--- aplikacija/app/src/main/java/com/bkl/air/foi/mdrivingschool/LoginActivity.java
+++ aplikacija/app/src/main/java/com/bkl/air/foi/mdrivingschool/LoginActivity.java
@@ -2,12 +2,36 @@ package com.bkl.air.foi.mdrivingschool;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.bkl.air.foi.mdrivingschool.helpers.LoginData;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    String login_name, login_pass;
+
+    @BindView(R.id.editText_username)
+    EditText username;
+
+    @BindView(R.id.editText_password)
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button_prijava)
+    public void onButtonPrijavaClick(){
+        login_name = username.getText().toString();
+        login_pass = password.getText().toString();
+        LoginData loginData = new LoginData(this);
+        loginData.execute("login",login_name,login_pass);
     }
 }
