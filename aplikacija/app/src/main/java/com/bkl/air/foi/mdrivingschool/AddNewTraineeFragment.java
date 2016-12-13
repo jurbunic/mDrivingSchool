5c8830ddf94638f7209180a34c3ade38d31f6f79
matlazar <matijalazar@yahoo.com>
2016-12-13 08:57:54 +0100

    Dodana metoda onButtonAddTraineeClick
    Navedena metoda je dodana u fragment AddNewTreineeFragment

--- aplikacija/app/src/main/java/com/bkl/air/foi/mdrivingschool/AddNewTraineeFragment.java
+++ aplikacija/app/src/main/java/com/bkl/air/foi/mdrivingschool/AddNewTraineeFragment.java
@@ -8,14 +8,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
+import com.bkl.air.foi.mdrivingschool.helpers.RetriveData;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Dalibor on 12.12.2016..
  */
 
 public class AddNewTraineeFragment extends Fragment {
+
+    String sName, sSurname, sBornDate, sBornPlace, sMobile, sPhone, sEmail,sAdress, sUsername, sPassword;
 
     @BindView(R.id.editText_name_p)
     EditText name;
@@ -58,5 +63,22 @@ public class AddNewTraineeFragment extends Fragment {
     public void onStart(){
         super.onStart();
         ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Dodavanje novog polaznika");
+    }
+
+    @OnClick(R.id.button_add_trainee)
+    public void onButtonAddTraineeClick(){
+        sName = name.getText().toString();
+        sSurname = surname.getText().toString();
+        sBornDate = bornDate.getText().toString();
+        sBornPlace = bornPlace.getText().toString();
+        sMobile = mobile.getText().toString();
+        sPhone = phone.getText().toString();
+        sEmail = email.getText().toString();
+        sAdress = adress.getText().toString();
+        sUsername = username.getText().toString();
+        sPassword = password.getText().toString();
+
+        RetriveData retriveData = new RetriveData(getActivity().getApplicationContext());
+        retriveData.execute("4","1",sName,sSurname,sBornDate,sBornPlace,sMobile,sPhone,sEmail,sAdress,sUsername,sPassword);
     }
 }
