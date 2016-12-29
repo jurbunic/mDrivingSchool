156c9f289f9cbb0f125b0e5168f0c10947c84477
dalkofjac <dadolg22@gmail.com>
2016-12-29 21:11:24 +0100

    Nadopuna dodjele polaznika instruktoru (oduzimanje)
    -Napravljeno grafičko sučelje i početna implementacija mogućnosti
    brisanja dodjeljenih polaznika instruktoru
    - Malo rekonstruiran kod u AssignTraineetoEmployeeFragmentu

--- aplikacija/app/src/main/java/com/bkl/air/foi/mdrivingschool/employee_fragments/AssignTraineeToEmployeeFragment.java
+++ aplikacija/app/src/main/java/com/bkl/air/foi/mdrivingschool/employee_fragments/AssignTraineeToEmployeeFragment.java
@@ -12,6 +12,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bkl.air.foi.database.Korisnik;
 import com.bkl.air.foi.mdrivingschool.R;
@@ -37,10 +38,19 @@ public class AssignTraineeToEmployeeFragment extends Fragment implements Adapter
     @BindView(R.id.textView_dp_employee_name)
     TextView employeeNameTextView;
 
+    @BindView(R.id.textView_dp_employee_name2)
+    TextView employeeNameTextViewSecond;
+
     @BindView(R.id.spinner_ip)
     Spinner traineesSpinner;
 
-    String chosenTrainee = "";
+    @BindView(R.id.spinner_ip_second)
+    Spinner traineesSpinnerSecond;
+
+    String currentUserId = "";
+
+    String chosenTraineeForAssigning = "";
+    String chosenTraineeForUnassigning = "";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
@@ -58,31 +68,58 @@ public class AssignTraineeToEmployeeFragment extends Fragment implements Adapter
         UserInfo info = new UserInfo(getActivity().getApplicationContext());
         Korisnik korisnik = info.getInfoById(getActivity().getIntent().getStringExtra("USER"));
 
+        //U ovoj se varijabli sprema id prijavljenog zaposlenika da bude dohvativ u svim metodama
+        currentUserId = getActivity().getIntent().getStringExtra("USER");
+
         employeeNameTextView.setText(korisnik.getIme()+" "+korisnik.getPrezime());
+        employeeNameTextViewSecond.setText(korisnik.getIme()+" "+korisnik.getPrezime());
 
         traineesSpinner.setOnItemSelectedListener(this);
+        traineesSpinnerSecond.setOnItemSelectedListener(this);
 
         List<String> allTraineesNames = new ArrayList<String>();
-
         ArrayList<Korisnik> allTrainees = new ArrayList<>();
+
+        List<String> allTraineesNamesForUnassagning = new ArrayList<String>();
+        ArrayList<Korisnik> allTraineesForUnassagning = new ArrayList<>();
 
         //Dohvaćaju se podaci o svim polaznicima
         allTrainees = info.getFreeTrainees(getActivity().getIntent().getStringExtra("USER"));
+
+        allTraineesForUnassagning = info.getTrainees(getActivity().getIntent().getStringExtra("USER"));
 
         //Spremaju se id-jevi, imena i prezimena u privremenu listu koju vidi korisnik na spinneru
         for (Korisnik trainee : allTrainees){
             allTraineesNames.add(trainee.getId() + " - " + trainee.getIme() + " " + trainee.getPrezime());
         }
 
+        for (Korisnik trainee : allTraineesForUnassagning){
+            allTraineesNamesForUnassagning.add(trainee.getId() + " - " + trainee.getIme() + " " + trainee.getPrezime());
+        }
+
         //Postavlja se adapter za prikaz imena polaznika u spinneru
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(thisContext, android.R.layout.simple_spinner_item, allTraineesNames);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         traineesSpinner.setAdapter(dataAdapter);
+
+        ArrayAdapter<String> dataAdapterSecond = new ArrayAdapter<String>(thisContext, android.R.layout.simple_spinner_item, allTraineesNamesForUnassagning);
+        dataAdapterSecond.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        traineesSpinnerSecond.setAdapter(dataAdapterSecond);
     }
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String item = parent.getItemAtPosition(position).toString();
-        chosenTrainee = item;
+        switch (parent.getId()){
+            case R.id.spinner_ip:
+                String item = parent.getItemAtPosition(position).toString();
+                chosenTraineeForAssigning = item;
+                break;
+            case R.id.spinner_ip_second:
+                String itemSecond = parent.getItemAtPosition(position).toString();
+                chosenTraineeForUnassigning = itemSecond;
+                break;
+            default:
+                break;
+        }
     }
     public void onNothingSelected(AdapterView<?> arg0) {
     }
@@ -92,14 +129,19 @@ public class AssignTraineeToEmployeeFragment extends Fragment implements Adapter
 
         //Dobivanje id-a iz cijelog stringa, sada chosenTraineeId sadrži id odabranog polaznika
         String chosenTraineeId = "";
-        if(chosenTrainee.contains(" ")){
-            chosenTraineeId= chosenTrainee.substring(0, chosenTrainee.indexOf(" "));
-            UserInfo info = new UserInfo(getActivity().getApplicationContext());
-            Korisnik korisnik = info.getInfoById(getActivity().getIntent().getStringExtra("USER"));
-            String instruktor = Integer.toString(korisnik.getId());
-            RetriveData retriveData = new RetriveData(thisContext);
-            retriveData.execute("6","1",instruktor.toString(),chosenTraineeId);
+        if(chosenTraineeForAssigning.contains(" ")){
+            chosenTraineeId= chosenTraineeForAssigning.substring(0, chosenTraineeForAssigning.indexOf(" "));
         }
+        RetriveData retriveData = new RetriveData(thisContext);
+        retriveData.execute("6","1",currentUserId,chosenTraineeId);
 
+    }
+    @OnClick(R.id.button_unassign_trainee)
+    public void onUnassignButtonPressed(){
+        String chosenTraineeId = "";
+        if(chosenTraineeForUnassigning.contains(" ")){
+            chosenTraineeId= chosenTraineeForUnassigning.substring(0, chosenTraineeForUnassigning.indexOf(" "));
+        }
+        Toast.makeText(thisContext,"Instruktor id: "+currentUserId+", polaznik id: "+chosenTraineeId,Toast.LENGTH_SHORT).show();
     }
 }
